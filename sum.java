b242cbe5320950fc98900e7a26821b308e8fe5e8
vishalvsr67 <vishalsingh.vr04@gmail.com>
2024-12-10 22:40:27 +0530

    total code written

--- sum.java
+++ sum.java
@@ -1,8 +1,11 @@
-public class sum{
-    public static void main(String[] args);
+import java.util.*;
+class sum{
+    public static void main(String[] args){
     //input
     Scanner sc = new Scanner(System.in);
-    
-
-
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int sum = a + b;
+    System.out.println(sum);
+    }
 }
