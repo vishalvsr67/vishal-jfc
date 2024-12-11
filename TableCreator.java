116f4863e5a4b7e8ca56347789cd95c6f925b3c3
vishalvsr67 <vishalsingh.vr04@gmail.com>
2024-12-12 00:46:37 +0530

    created a code for creating the table of a given number

---
+++ TableCreator.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class TableCreator{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number which you want the table of it: ");
+        double number = scanner.nextDouble();
+        double r1answer = number * 1;
+        double r2answer = number * 2;
+        double r3answer = number * 3;
+        double r4answer = number * 4;
+        double r5answer = number * 5;
+        double r6answer = number * 6;
+        double r7answer = number * 7;
+        double r8answer = number * 8;
+        double r9answer = number * 9;
+        double r10answer = number * 10;
+
+        System.out.println(r1answer);
+        System.out.println(r2answer);
+        System.out.println(r3answer);
+        System.out.println(r4answer);
+        System.out.println(r5answer);
+        System.out.println(r6answer);
+        System.out.println(r7answer);
+        System.out.println(r8answer);
+        System.out.println(r9answer);
+        System.out.println(r10answer);
+
+
+    }
+}
