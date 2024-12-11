6642e93d6639c473cc4ab9e2d0244336739f1f9a
vishalvsr67 <vishalsingh.vr04@gmail.com>
2024-12-11 20:12:23 +0530

    finding the area of a circle problem hwp2

---
+++ area.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class area{
+    public static void main(String[] args){
+        double PI = 3.14;
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        double area = (PI*r*r);
+        System.out.println(area);
+    }
+}
