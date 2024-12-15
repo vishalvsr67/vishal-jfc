c91478b1841b2b9c2e6a1caa738f27fbc593a09d
vishalvsr67 <vishalsingh.vr04@gmail.com>
2024-12-15 19:10:45 +0530

    egl sloved

---
+++ conditionegl.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+class conditionegl{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a == b){
+            System.out.println("equal");
+       }
+       else{
+        if(a > b){
+             System.out.println("greater");
+        }
+        else{     
+            if(a < b)
+            System.out.println("lesser");
+           }
+       }
+    }
+}
+    
