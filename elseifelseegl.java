675366810a5769a209ffc22e94d5f62d560d0616
vishalvsr67 <vishalsingh.vr04@gmail.com>
2024-12-16 00:17:43 +0530

    elseifelse statements used to solve the same egl problem

---
+++ elseifelseegl.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class elseifelseegl{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a == b){
+            System.out.println("equal");
+       }
+       else if(a > b){
+             System.out.println("a is greater");
+        }
+        else{     
+            if(a < b)
+            System.out.println("a is lesser");
+           }
+     }
+}
+
+    
