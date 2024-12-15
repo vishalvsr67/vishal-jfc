89f262c1002a9cadb7f3b459893855b3f934d598
vishalvsr67 <vishalsingh.vr04@gmail.com>
2024-12-15 18:52:41 +0530

    odd even problem using ifelse condition solved

---
+++ conditionoddeven.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class conditionoddeven{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        if(x%2==0){
+            System.out.println("The given number is even number");
+        }
+        else{
+            System.out.println("The given number is odd number");
+        }
+
+    }
+
+}
+
