b2927243bd6e66878e33708b4e45c88a2fb67877
vishalvsr67 <vishalsingh.vr04@gmail.com>
2024-12-16 00:42:07 +0530

    button greeting system using switch case statement

---
+++ switchbutton.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class switchbutton{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int button = sc.nextInt();
+        
+    switch(button) {
+        case 1: System.out.println("Hello");
+        break;
+        case 2: System.out.println("Namaste");
+        break;
+        case 3: System.out.println("Bonjour");
+        break;
+        }
+    }
+}
+        
+
+    
