c95553596fab9a9b7b7047c27292bb006389445b
vishalvsr67 <vishalsingh.vr04@gmail.com>
2025-02-01 16:05:15 +0530

    Loops problem tried with counter 100

--- Loops.java
+++ Loops.java
@@ -1,6 +1,6 @@
 public class Loops {
     public static void main(String[] args){
-        for(int counter = 0; counter < 3; counter = counter + 1){
+        for(int counter = 0; counter < 100; counter = counter + 1){
             System.out.println("Hello World");
         }
     }
