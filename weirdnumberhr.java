2a15100780277d46f4bbb0799977485fd7166b65
vishalvsr67 <vishalsingh.vr04@gmail.com>
2025-02-01 14:30:06 +0530

    hackerrank problem had a code change

--- weirdnumberhr.java
+++ weirdnumberhr.java
@@ -15,7 +15,7 @@ public class weirdnumberhr{
         if(n%2!=0){
             System.out.println("Weird");
         }
-        else if(n>2 && n<5 && n%2==0){
+        else if(n>=2 && n<=5 && n%2==0){
             System.out.println("Not Weird");
         }
         else if(n>=6 && n<=20 && n%2==0){
