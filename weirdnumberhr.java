1c62c979c90c2e931a74bcd1dbeffedf463697bc
vishalvsr67 <vishalsingh.vr04@gmail.com>
2025-02-01 14:19:49 +0530

    hackerrank problem had a last else statement pending

--- weirdnumberhr.java
+++ weirdnumberhr.java
@@ -15,7 +15,7 @@ public class weirdnumberhr{
         if(n%2!=0){
             System.out.println("Weird");
         }
-        else if(n>=2 && n<=5 && n%2==0){
+        else if(n>2 && n<5 && n%2==0){
             System.out.println("Not Weird");
         }
         else if(n>=6 && n<=20 && n%2==0){
@@ -24,7 +24,10 @@ public class weirdnumberhr{
         else if(n>20 && n<=100 && n%2==0){
             System.out.println("Not Weird");
         }
-    
+        else{
+            System.out.println("number choosed is out of range");
+        }
     }
 }
 
+        
