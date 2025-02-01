cb79222bb35e60ac6e4ff84a4fea1daf4ce82139
vishalvsr67 <vishalsingh.vr04@gmail.com>
2025-02-01 16:40:58 +0530

    Loops problem tried with counter in the same line

---
+++ Loopssl.java
@@ -0,0 +1,9 @@
+public class Loopssl {
+    public static void main(String[] args){
+        for(int counter = 0; counter < 11; counter++){
+            System.out.print("counter");
+        }
+    }
+}
+    
+
