d9b76f54a7e4031aaebd5fbdf35cb9a13bf5441f
vishalvsr67 <vishalsingh.vr04@gmail.com>
2025-02-01 14:04:56 +0530

    hackerrank problem solved

--- weirdnumberhr.java
+++ weirdnumberhr.java
@@ -1,24 +1,30 @@
-import java.util.Scanner;
+import java.util.*;
+
 public class weirdnumberhr{
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
         int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
         scanner.close();
         
         if(n%2!=0){
             System.out.println("Weird");
         }
-        else if(n>2 && n<5 && n%2==0){
+        else if(n>=2 && n<=5 && n%2==0){
             System.out.println("Not Weird");
         }
-        else if(n>6 && n<20 && n%2==0){
+        else if(n>=6 && n<=20 && n%2==0){
             System.out.println("Weird");
         }
-        else if(n>20 && n<100 && n%2==0){
+        else if(n>20 && n<=100 && n%2==0){
             System.out.println("Not Weird");
         }
-        else{
-            System.out.println("number opted for checking is out of range");
-        }
+    
     }
 }
+
