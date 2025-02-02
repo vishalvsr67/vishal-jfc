1e7d84bd12e9096dec1f3fd30b1cf0878645a5f7
vishalvsr67 <vishalsingh.vr04@gmail.com>
2025-02-02 23:59:30 +0530

    constraints added and i learnt using two for loops at a time

--- loopsiihr.java
+++ loopsiihr.java
@@ -3,25 +3,34 @@ import java.util.*;
 public class loopsiihr {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int t = in.nextInt();  // Number of queries
+        int q = in.nextInt();  // Number of queries
 
-        
-        // Loop for each query
-        for (int q = 0; q < t; q++) {
+        if(q >= 0 || q <= 500){
+            // Loop for each query
+            for (int i = 0; i < q; i++) {
             int a = in.nextInt();
             int b = in.nextInt();
             int n = in.nextInt();
             
             int sum = a;  // Start sum with 'a'
-            
-            // Generate series
-            for (int i = 0; i < n; i++) {
+            if(a >= 0 || a <= 50 || b >= 0 || b <= 50 || n >= 1 || n <= 15){
+                // Generate series
+                for (int j = 0; j < n; j++) {
                 sum += Math.pow(2, i) * b;  // Compute term
                 System.out.print(sum + " ");  // Print in same line
             }
-            System.out.println();  // Move to new line after each query
-        }
+            System.out.println();  // Move to new line after each query}
+            }
+            else{
+                System.out.println("Invalid range choosen!");
+            }
         
-        in.close();  // Close scanner
+            
+        }
     }
+    else{
+        System.out.println("Invalid range choosen");
+    }
+    in.close();  // Close scanner
+}
 }
