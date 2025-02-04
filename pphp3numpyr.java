b488f79ef81c2642de04b1e870d594740d0b7337
vishalvsr67 <vishalsingh.vr04@gmail.com>
2025-02-05 01:09:48 +0530

    im not able to solve it as its late now ill do it tommorow

---
+++ pphp3numpyr.java
@@ -0,0 +1,21 @@
+public class pphp3numpyr {
+    public static void main(String[] args) {
+        int n = 5;
+        int m = 5;
+        //outer loop
+        for(int i = 1; i <= n; i++){
+            //inner loop
+            for(int j = 1; j <= n-i; j++){
+                System.out.print("  ");
+            }
+            //inner loop
+            for(int j = m-i-1; j >= i; j--){
+                System.out.print(j+" ");
+            }
+            for(int j = m-i+1; j <= m; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
