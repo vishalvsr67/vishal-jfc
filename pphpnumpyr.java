c0b008071abf6ed63052edd062cc2fbf5cd70776
vishalvsr67 <vishalsingh.vr04@gmail.com>
2025-02-05 00:00:59 +0530

    actually the number was getting incremented so we removed the increment

--- pphpnumpyr.java
+++ pphpnumpyr.java
@@ -1,17 +1,15 @@
-public class pphpnumpyr{
+public class pphpnumpyr {
     public static void main(String[] args) {
         int n = 5;
-        int number = 1;
-        //outer loop
-        for(int i=1; i<=n; i++){
-            //inner loop -> for spaces
-            for(int j=1; j<=n-i;j++){
+        // outer loop
+        for (int i = 1; i <= n; i++) {
+            // inner loop -> for spaces
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            //inner loop -> for printing numbers
-            for(int j=1; j<=i; j++){
-                System.out.print(number);
-                number = number + i;
+            // inner loop -> for printing numbers
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
             }
             System.out.println();
         }
