3350e49fbb7324ea0059ec37b2572db1b6b23c43
vishalvsr67 <vishalsingh.vr04@gmail.com>
2025-02-20 01:20:17 +0530

    i got the code correct by the help of chatgpt

--- pphollowrhombus.java
+++ pphollowrhombus.java
@@ -1,28 +1,28 @@
-public class pphollowrhombus{
+import java.util.Scanner;
+
+public class pphollowrhombus   {
     public static void main(String[] args) {
-        int n = 5;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+        sc.close();
         for(int i = 1; i <= n; i++){
             //spaces
             for(int j = 1; j <= n-i; j++){
                 System.out.print(" ");
             }
+            //first and last row fully filled  
             if(i == 1 || i == n){
-                for(int j = 1; j <= 5; j++){
+                for(int j = 1; j <= n; j++){
                     System.out.print("*"+" ");
                 }
-            } else if(i > 1 || i < n){
-                for(int j = 1; j <= i; j++){
-                    System.out.print("*");
-                }
-                
-                //spaces
-                int spaces = (n - 2);
-                for(int j = 1; j <= spaces; j++){
-                    System.out.print(" ");
-                }
-                for(int j = 1; j <= i; j++){
-                    System.out.print("*");
-                }
+            } else {
+                // Hollow middle part
+            System.out.print("*"); // First star
+            for (int j = 1; j <= n - 2; j++) {
+                System.out.print("  ");
+            }
+            System.out.print(" *"); // Last star
             }
             System.out.println();
         }
