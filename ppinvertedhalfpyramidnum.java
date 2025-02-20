05d8b3b6c76152fd259081297b4e1e0e18a9f299
vishalvsr67 <vishalsingh.vr04@gmail.com>
2025-02-21 02:12:55 +0530

    im getting stuck ill get back it tommorow i mean today agian

--- ppinvertedhalfpyramidnum.java
+++ ppinvertedhalfpyramidnum.java
@@ -1,11 +1,12 @@
 public class ppinvertedhalfpyramidnum {
     public static void main(String[] args) {
         int n = 4;
-        for(int i = 1; i <= n; i++){
+        for(int i = n; i >= 1; i--){
             //print the numbers in inverted format
-            int num = 1;
-            for(int j = n; j >= 1; j++){
-                System.out.print(num);
+            
+            for(int j = 1; j <= i; j++){
+                System.out.print(j);
+              
             }
             System.out.println();
         }
