dcb5d4ee26bde0720560c065690d2363576f6a95
vishalvsr67 <vishalsingh.vr04@gmail.com>
2025-02-21 00:23:57 +0530

    actually the code which chatgpt gave was using the topics which im not familiar with so i created my code using the loops concept

--- pppascalstriangle.java
+++ pppascalstriangle.java
@@ -1,40 +1,24 @@
 import java.util.Scanner;
 
-public class pppascalstriangle {
-    public static void printPascalsTriangle(int n) {
-        int[][] triangle = new int[n][n];
-
-        // Filling Pascal's Triangle
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                if (j == 0 || j == i) {
-                    triangle[i][j] = 1;
-                } else {
-                    triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
-                }
-            }
-        }
-
-        // Printing Pascal's Triangle
-        for (int i = 0; i < n; i++) {
-            // Printing spaces for formatting
-            for (int k = 0; k < n - i; k++) {
-                System.out.print(" ");
-            }
-            // Printing numbers
-            for (int j = 0; j <= i; j++) {
-                System.out.print(triangle[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
+public class ppPascalsTriangle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of rows: ");
         int n = scanner.nextInt();
         scanner.close();
-        
-        printPascalsTriangle(n);
+
+        for (int i = 0; i < n; i++) {
+            // Spaces for formatting
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+
+            int num = 1;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(num + " ");
+                num = num * (i - j) / (j + 1); // Pascal's triangle formula
+            }
+            System.out.println();
+        }
     }
 }
