d748c00521dcfda33f5aee693013f1bcb401165c
vishalvsr67 <vishalsingh.vr04@gmail.com>
2025-02-21 01:09:26 +0530

    so i finally got my code done right

--- pphalfpyramidmytryvl6.java
+++ pphalfpyramidmytryvl6.java
@@ -7,10 +7,9 @@ public class pphalfpyramidmytryvl6 {
                 System.out.print(" ");
             }
             //print the numbers
-            int num = 0;
+            
             for(int j = 1; j <= i; j++){
-                System.out.print(j + num);
-                num++;
+                System.out.print(j+" ");
             }
             System.out.println();
         }
