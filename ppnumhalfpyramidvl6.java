342bef2a892a95975cfb1785430700cf53f457b0
vishalvsr67 <vishalsingh.vr04@gmail.com>
2025-02-21 00:58:46 +0530

    finally done with the code

--- ppnumhalfpyramidvl6.java
+++ ppnumhalfpyramidvl6.java
@@ -3,8 +3,9 @@ public class ppnumhalfpyramidvl6{
         int n = 5;
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= i; j++){
-                System.out.print("j");
+                System.out.print(j);
             }
+            System.out.println();
         }
     }
 }
