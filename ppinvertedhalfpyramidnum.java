99ec19074e5ccbb4baf12a24a050948367fddf0d
vishalvsr67 <vishalsingh.vr04@gmail.com>
2025-02-21 02:27:29 +0530

    so finally got the code correct

--- ppinvertedhalfpyramidnum.java
+++ ppinvertedhalfpyramidnum.java
@@ -1,15 +1,14 @@
 public class ppinvertedhalfpyramidnum {
     public static void main(String[] args) {
         int n = 4;
-        for(int i = n; i >= 1; i--){
+        for(int i = 1; i <= n; i++){
             //print the numbers in inverted format
             
-            for(int j = 1; j <= i; j++){
-                System.out.print(j);
+            for(int j = 1; j <= n - i + 1; j++){
+                System.out.print(i);
               
             }
             System.out.println();
         }
     }
-    
 }
