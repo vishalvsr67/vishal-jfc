86122044b6b3f770d6f6913e121c1e12c54f9da4
vishalvsr67 <vishalsingh.vr04@gmail.com>
2025-02-23 00:09:22 +0530

    made changes in the code and made the code right

--- funprintfactorial.java
+++ funprintfactorial.java
@@ -1,6 +1,24 @@
-import java.util.*;
+
+import java.util.Scanner;
+
 public class funprintfactorial {
     public static void printFactorial(int n){
+        //loops
+        if(n<0){
+            System.out.println("invalid number");
+        }else{int factorial = 1;
+            for(int i = n; i >= 1; i--){
+                factorial = factorial + i;
+            }
+            System.out.println(factorial);
+            return;
+        }
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
+        printFactorial(n);
     }
 }
