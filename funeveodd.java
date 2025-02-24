90bde9d8bc45edda3028a4def95ba554b7887f17
vishalvsr67 <vishalsingh.vr04@gmail.com>
2025-02-24 22:46:35 +0530

    program for finding whether a given number is an even number or odd number

---
+++ funeveodd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class funeveodd {
+    public static int checkIfEveOdd(int n) {
+        if(n % 2 == 0){
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
+        }
+
+        public static void main(String[] args){
+            
+
+        }
+    }
+    
+}
