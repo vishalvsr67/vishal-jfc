84583bb44263275871f708eb49fb7f9068e7c223
vishalvsr67 <vishalsingh.vr04@gmail.com>
2025-02-26 01:10:39 +0530

    finally got my code right and executed how to print table for any given number

--- funtableofgivennum.java
+++ funtableofgivennum.java
@@ -1,5 +1,17 @@
+import java.util.*;
 public class funtableofgivennum {
-    public static void printTable (int n) {
+    public static void printTable (int n, int j) {
+        for(int i = 1; i <= j; i++){
+            System.out.println(n + "x" + i + "=" + (n*i));
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of which you want to print the table: ");
+        int n = sc.nextInt();
+        System.out.println("Enter the number upto which you want to the print the table of the chosen number: ");
+        int j = sc.nextInt();
         
+        printTable(n,j);
     }
 }
